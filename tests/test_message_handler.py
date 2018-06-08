272a74d64d9dec936e707a6b8ac3dcc82088f916
hacknaked <fcmiranda@gmail.com>
2018-06-08 09:44:43 -0300

    Added unittest for message_handler basic features.

--- tests/test_message_handler.py
+++ tests/test_message_handler.py
@@ -1,2 +1,35 @@
 # -*- coding: utf-8 -*-
+import unittest
 
+from lucena.message_handler import MessageHandler
+
+
+class TestMessageHandler(unittest.TestCase):
+    @staticmethod
+    def basic_handler():
+        pass
+
+    def setUp(self):
+        super(TestMessageHandler, self).setUp()
+
+    def test_more_properties_win(self):
+        mh1 = MessageHandler(
+            {'a': 123, 'b': 999, 'c': 'hello'},
+            self.basic_handler
+        )
+        mh2 = MessageHandler(
+            {'a': 123, 'b': 999},
+            self.basic_handler
+        )
+        self.assertTrue(mh1 < mh2)
+
+    def test_alphabetical_order_win(self):
+        mh1 = MessageHandler(
+            {'a': 123, 'b': 999, 'c': 'hello'},
+            self.basic_handler
+        )
+        mh2 = MessageHandler(
+            {'a': 123, 'b': 999, 'd': 'hello'},
+            self.basic_handler
+        )
+        self.assertTrue(mh1 < mh2)
