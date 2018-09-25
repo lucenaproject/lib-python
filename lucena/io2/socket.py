54c3356aaa1d55b81ddbf4717eae990da6212100
Fernando Miranda <fernandom@disarmista.com>
2018-09-25 08:43:06 -0300

    Socker simplified

--- lucena/io2/socket.py
+++ lucena/io2/socket.py
@@ -6,24 +6,19 @@ import zmq
 
 
 class Socket(zmq.Socket):
-    MAGIC = 0x7d9afffe00000000
-    SIGNAL_READY = 1
-    SIGNAL_STOP = 2
+    SIGNAL_READY = 0x7f000001
+    SIGNAL_STOP = 0x7f000002
+
+    @staticmethod
+    def is_signal(message):
+        return len(message) == 4 and message[3] == 0x7f
 
     def signal(self, status=0):
-        assert status < 1000
-        self.send(struct.pack("Q", Socket.MAGIC + status))
+        assert status < 0x7fffffff
+        self.send(struct.pack("I", status))
 
     def wait(self, timeout=-1):
         self.setsockopt(zmq.RCVTIMEO, timeout)
-        try:
-            message = self.recv()
-        except zmq.ZMQError:
-            message = ''
-
-        if not len(message) == 8:
-            return None
-        signal_value = struct.unpack('Q', message)[0]
-        if (signal_value & 0xffffffff00000000) == Socket.MAGIC:
-            return signal_value & 0xffffffff
-        return None
+        message = self.recv()
+        assert Socket.is_signal(message)
+        return struct.unpack('I', message)[0]
