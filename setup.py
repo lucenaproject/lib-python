ddf0f80c10ad0ed39134def6c8eac34d7f5b4884
hacknaked <fcmiranda@gmail.com>
2018-11-09 09:16:15 -0300

    Updated version to 0.6 pre-alpha release

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 
 setup(
     name='lucena',
-    version='0.5.1',
-    description='Microservices library for Lucena Project',
-    url='https://github.com/lucenaproject/lucena.py',
+    version='0.6',
+    description='Python core library for building web services in Lucena Project.',
+    url='https://github.com/lucenaproject',
     author='Fernando Miranda',
     author_email=''.join(['fcmiranda', '@', 'gmail', '.', 'com']),
     license='MIT',
